14670014c26fe36490b09b508e862a82489e1836
Lucian Nguyen <lucian.n@seznam.cz>
2022-02-05 13:34:11 +0100

    game finished gotta work on the ghost movement

--- js/game.js
+++ js/game.js
@@ -1,35 +1,32 @@
 // Hero position modifier and variables
 const hero = document.querySelector('.hero');
 
-let heroPosY = 500;
-let heroPosX = 100;
-hero.style.top = heroPosY + "px";
-hero.style.left = heroPosX + "px";
+    let heroPosY = 500;
+    let heroPosX = 100;
+    
+    hero.style.top = heroPosY + "px";
+    hero.style.left = heroPosX + "px";
 
 // Ghost position modifier and variables
 const ghost = document.querySelector('.ghost');
 
-let ghostPosY = 200;
-let ghostPosX = 600;
+    let ghostPosY = 200;
+    let ghostPosX = 600;
 
-ghost.style.top = ghostPosY + "px";
-ghost.style.left = ghostPosX + "px";
+    ghost.style.top = ghostPosY + "px";
+    ghost.style.left = ghostPosX + "px";
 
 // Treasure position modifier and variables
 const treasure = document.querySelector('.treasure');
 
-let treasurePosY = 0;
-let treasurePosX = 400;
+    let treasurePosY = 0;
+    let treasurePosX = 400;
 
-treasure.style.top = treasurePosY + 'px';
-treasure.style.left = treasurePosX + 'px';
+    treasure.style.top = treasurePosY + 'px';
+    treasure.style.left = treasurePosX + 'px';
 
 // Hero movement wsad or arrows + win/lose function
 document.addEventListener('keydown', (key) => {
-
-    
-    
-
 
     if ((key.code === 'ArrowUp' || key.code === "KeyW") && heroPosY > 0) {
         heroPosY -= 100;
@@ -66,23 +63,58 @@ const winningOrLosing = () => {
         console.log("he should win by now")
         alert("You have won")
     }
-
+    
     else if (heroPosX === ghostPosX && heroPosY === ghostPosY) {
         alert("You died. Git gud scrub")
     }
 }
 
 // fucking ghost movement increments
-const ghostMoveIncrement = Math.round(Math.random())
 
+/*this equation always gives the same number. Dont know why. */
+
+// let ghostMoveRandomizer(min, max) {
+    //     return Math.random() *
+    
+    // }
+    
+
+// setting interval for ghost movement
+    const ghostInterval = setInterval(() => {
+    
+        console.log("watch out for the ghost")
+        console.log(ghostMoveIncrement)
+        ghostMove()
+    }, 1000)
+
+    
+    
+    
+    let ghostMoveIncrement = Math.floor(Math.random()*2)
+    
+    // ghost only moves diagonally
 const ghostMove = () => {
     switch (ghostMoveIncrement) {
         case 1:
+            ghost.style.top = ghostPosY + "px";
+            ghost.style.left = ghostPosX + "px";
+            ghostPosX += 100;
             ghostPosY += 100;
             break;
         case 0:
+            ghost.style.top = ghostPosY + "px";
+            ghost.style.left = ghostPosX + "px";
             ghostPosX -= 100;
+            ghostPosY -= 100;
             break;
-     }    
+    }
+        
+// Daniels randomizer in range function
 
+function getRandomInt(min, max) {
+    min = Math.ceil(-100);
+    max = Math.floor(100);
+     return Math.floor(Math.random() * (max - min + 1)) + min;
+                
+    }
 }
